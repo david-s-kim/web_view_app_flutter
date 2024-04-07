de915190e3c66ec73ce4912478bda71a72126b24
David Kim <mycodingtool9@gmail.com>
2024-04-07 14:17:45 +0900

    - feat: WebView 기본 설정

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -9,10 +9,18 @@ class MainScreen extends StatefulWidget {
 
 class _MainScreenState extends State<MainScreen> {
   @override
+  void initState() {
+    super.initState();
+    if (Plaform.isAndroid) WebView.platform = AndroidWebView();
+  }
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('나만의 웹르라우저'),
+      ),
+      body: WebView(
+        initialUrl: 'https://flutter.dev',
       ),
     );
   }
